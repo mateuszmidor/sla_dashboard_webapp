5fb9cdd167dde971ce0477400949c3c3d1be42c5
lukaa12 <lukasz.wolanin@codilime.com>
2021-05-17 14:09:29 +0200

    Swaped html table with plotly Heatmap with mouse-over event
    KNTK-230

--- presentation/matrix_view.py
+++ presentation/matrix_view.py
@@ -5,6 +5,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
 from dash.dependencies import Input, Output
+import numpy as np
 
 from domain.config import Config
 from domain.config.thresholds import Thresholds
@@ -12,69 +13,115 @@ from domain.model import MeshResults
 from presentation.matrix import Matrix
 
 
-def make_mesh_test_matrix_layout(mesh: MeshResults, config: Config) -> dash_table.DataTable:
-    column_zero_header = {"name": "", "id": "agent_alias"}  # left-top table cell
-    columns = [column_zero_header] + [{"name": row.agent_alias, "id": row.agent_alias} for row in mesh.rows]
+RED = 'rgb(255,0,0)'
+ORANGE = 'rgb(255,165,0)'
+GREEN = 'rgb(0,255,0)'
+
+
+def make_mesh_test_matrix_layout(mesh: MeshResults, config: Config) -> dcc.Graph:
     matrix = Matrix(mesh)
-    data = make_data(mesh, matrix)
-    styles = make_colors(mesh, config.latency)
-    return dash_table.DataTable(
-        id="table",
-        columns=columns,
-        style_cell={"textAlign": "center"},
-        data=data,
-        style_data_conditional=styles,
+    data = make_data(mesh, matrix, config.latency_deteriorated_ms, config.latency_failed_ms)
+    annotations = make_annotations(mesh, matrix)
+    layout = dict(
+        margin=dict(l=150, b=50, t=100, r=50),
+        modebar={"orientation": "v"},
+        annotations=annotations,
+        xaxis=dict(side="top", ticks="", scaleanchor="y"),
+        yaxis=dict(side="left", ticks=""),
+        hovermode="closest",
+        showlegend=False,
     )
 
+    return dcc.Graph(figure={'data': data, 'layout': layout}, style={"width": 750, "height": 750})
 
-def make_data(mesh: MeshResults, matrix: Matrix) -> List[Dict]:
-    data = []
-    for row in mesh.rows:
-        row_data = {"agent_alias": row.agent_alias}
+
+def make_data(mesh: MeshResults, matrix: Matrix, latency_deteriorated_ms: int, latency_failed_ms: int) -> List[Dict]:
+    x = matrix.agents
+    y = [ i for i in reversed(matrix.agents)]    
+    z = []
+
+    for row in reversed(mesh.rows):
+        z_col = []
         for col in row.columns:
-            latency_us = matrix.cells[row.agent_alias][col.agent_alias].latency_microsec.value
-            latency_ms = latency_us / 1000
-            row_data[col.agent_alias] = f"{latency_ms} ms"
-        data.append(row_data)
+            color = get_color(matrix.cells[row.alias][col.alias].latency_microsec.value, latency_deteriorated_ms, latency_failed_ms)
+            z_col.append(color)
+        z.append(z_col)
+    for i in range(len(mesh.rows)):
+        z[-(i + 1)].insert(i, None)
+    z = np.array(z)
+
+    colorscale = get_colorscale(z)
+
+    data = [
+        dict(
+            x=x,
+            y=y,
+            z=z,
+            text=make_hover_text(mesh, matrix),
+            type="heatmap",
+            hoverinfo='text',
+            opacity=1,
+            name="",
+            showscale=False,
+            colorscale=colorscale,
+        )
+    ]
+
     return data
 
 
-def make_colors(mesh: MeshResults, latency_thresholds: Thresholds):
-    styles = []
-    for index, row in enumerate(mesh.rows):
-        for column in row.columns:
-            threshold_failed_microsec = latency_thresholds.failed(row.agent_id, column.agent_id)
-            threshold_deteriorated_microsec = latency_thresholds.deteriorated(row.agent_id, column.agent_id)
+def make_annotations(mesh: MeshResults, matrix: Matrix) -> List[Dict]:
+    annotations = []
+    for row in reversed(mesh.rows):
+        for col in row.columns:
+            annotations.append(
+                dict(
+                    showarrow=False,
+                    text=f"<b>{(matrix.cells[row.alias][col.alias].latency_microsec.value * 1e-3):.2f} ms<b>",
+                    xref="x",
+                    yref="y",
+                    x=col.alias,
+                    y=row.alias,
+                )
+            )
+    return annotations
 
-            if column.latency_microsec.value > threshold_failed_microsec:
-                styles.append(
-                    {
-                        "if": {
-                            "column_id": column.agent_alias,
-                            "row_index": index,
-                        },
-                        "backgroundColor": "red",
-                    }
-                )
-            elif column.latency_microsec.value > threshold_deteriorated_microsec:
-                styles.append(
-                    {
-                        "if": {
-                            "column_id": column.agent_alias,
-                            "row_index": index,
-                        },
-                        "backgroundColor": "orange",
-                    }
-                )
-            else:
-                styles.append(
-                    {
-                        "if": {
-                            "column_id": column.agent_alias,
-                            "row_index": index,
-                        },
-                        "backgroundColor": "green",
-                    }
-                )
 
-    return styles
+def make_hover_text(mesh: MeshResults, matrix: Matrix) -> List[List[str]]:
+    text = []
+    for row in reversed(mesh.rows):
+        text_col = []
+        for col in row.columns:
+            latency_ms = matrix.cells[row.alias][col.alias].latency_microsec.value * 1e-3
+            jitter = matrix.cells[row.alias][col.alias].jitter.value
+            loss = matrix.cells[row.alias][col.alias].packet_loss.value
+            text_col.append(f"Latency: {latency_ms:.2f} ms, \nJiter: {jitter}, \nLoss: {loss}")
+        text.append(text_col)
+    for i in range(len(mesh.rows)):
+        text[-(i + 1)].insert(i, "")
+    
+    return text
+
+
+def get_color(val, latency_deteriorated_ms: int, latency_failed_ms: int):
+    if val < latency_deteriorated_ms:
+        return 0
+    if val < latency_failed_ms:
+        return 0.5
+    else:
+        return 1.0
+
+
+def get_colorscale(z: np.array) -> List[List]:
+    if np.all(z == 0.0):
+        return [[0.0, GREEN], [1.0, GREEN]]
+    if np.all(z == 0.5):
+        return [[0.0, ORANGE], [1.0, ORANGE]]
+    if np.all(z == 1.0):
+        return [[0.0, RED], [1.0, RED]]
+    if not np.any(z == 0.0):
+        return [[0.0, ORANGE], [1.0, RED]]
+    if not np.any(z == 1.0):
+        return [[0.0, GREEN], [1.0, ORANGE]]
+    else:
+        return [[0.0, GREEN], [0.5, ORANGE], [1.0, RED]]
