6abd81cc78e924c04d6349b6b64e2a60426cce5a
Mateusz Midor <3demaniac@gmail.com>
2021-06-18 13:56:08 +0200

    Change gunicorn worker_class from default sync to gthread

--- data/gunicorn.conf.py
+++ data/gunicorn.conf.py
@@ -1,6 +1,10 @@
 # For more settings, see: https://docs.gunicorn.org/en/stable/settings.html
 
+import multiprocessing
+
 workers = 1
 worker_connections = 100
 bind = ":8050"
 timeout = 30
+worker_class = "gthread"
+threads = 2 * multiprocessing.cpu_count() + 1  # this formula is suggested in gunicorn docs
