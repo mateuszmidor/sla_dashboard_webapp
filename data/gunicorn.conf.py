a4b115848106abf7d5e08d2927c910186eab0d05
Martin Machacek <m@m3a.net>
2021-06-20 12:00:18 -0700

    Update data/gunicorn.conf.py
    Co-authored-by: Daniel Furman <danielfurman8@gmail.com>

--- data/gunicorn.conf.py
+++ data/gunicorn.conf.py
@@ -6,5 +6,7 @@ workers = 1
 worker_connections = 100
 bind = ":8050"
 timeout = 30
+# Worker is changed to prevent worker timeouts
+# See: https://github.com/benoitc/gunicorn/issues/1801#issuecomment-585886471
 worker_class = "gthread"
 threads = 2 * multiprocessing.cpu_count() + 1  # this formula is suggested in gunicorn docs
