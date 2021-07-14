26bf24e3744419be428a9406c7b53c101ee9cd55
Daniel Furman <daniel.furman@codilime.com>
2021-07-14 02:00:38 +0200

    Do not change the group of generated files (#48)
    See: https://github.com/kentik/community_sdk_golang/pull/43#discussion_r667137412
    Issue: KNTK-283

--- generate_client.sh
+++ generate_client.sh
@@ -4,7 +4,7 @@
 
 
 function run() {
-    checkPrerequsites
+    check_prerequisites
 
     # GENERATE SYNTHETICS
     synthetics_package="generated.synthetics_http_client.synthetics"
@@ -21,14 +21,14 @@ function stage() {
     BOLD_BLUE="\e[1m\e[34m"
     RESET="\e[0m"
     msg="$1"
-    
+
     echo
     echo -e "$BOLD_BLUE$msg$RESET"
 }
 
-function checkPrerequsites() {
+function check_prerequisites() {
     stage "Checking prerequisites"
-    
+
     if ! docker --version > /dev/null 2>&1; then
         echo "You need to install docker to run the generator"
         exit 1
@@ -51,15 +51,15 @@ function generate_golang_client_from_openapi3_spec() {
         --package-name "$package" \
         --additional-properties generateSourceCodeOnly=true \
         -o "/local/$output_dir"
-    
+
     echo "Done"
 }
 
 function change_ownership_to_current_user() {
     dir="$1"
-    stage "Changing ownership of $dir to $USER:$USER"
+    stage "Changing ownership of $dir to $USER"
 
-    sudo chown  -R "$USER:$USER"  "$dir" # by default the generated output is in user:group root:root
+    sudo chown -R "$USER" "$dir" # by default the generated output is in user:group root:root
 
     echo "Done"
 }
