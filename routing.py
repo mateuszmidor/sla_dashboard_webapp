c098487b272f2e4cf44d11aeb572bc97a8fdae77
Mateusz Midor <3demaniac@gmail.com>
2021-10-14 09:30:17 +0200

    Move more of routing related code to routing.py

--- routing.py
+++ routing.py
@@ -1,19 +1,39 @@
 import logging
 import urllib.parse as urlparse
+from enum import Enum
 from typing import Tuple
 
 from domain.metric import MetricType
 from domain.types import AgentID
 
-MAIN = "/"
-MATRIX = "/matrix"
-TIME_SERIES = "/time-series"
-
 logger = logging.getLogger("routing")
 
 
+class Route(Enum):
+    INDEX = "/"
+    MATRIX = "/matrix"
+    TIME_SERIES = "/time-series"
+    UNKNOWN = "[unknown_route]"
+
+
+def extract_route(pathname: str) -> Route:
+    """
+    Example:
+        pathname: /matrix?metric=Latency
+        return:   Route.MATRIX
+    """
+
+    route_end_index = pathname.find("?")
+    route_str = pathname[:route_end_index] if route_end_index != -1 else pathname
+    try:
+        return Route(route_str)
+    except Exception:
+        logger.exception("Unknown route: %s", route_str)
+        return Route.UNKNOWN
+
+
 def encode_matrix_path(metric: MetricType) -> str:
-    return f"{MATRIX}?metric={metric.value}"
+    return f"{Route.MATRIX.value}?metric={metric.value}"
 
 
 def decode_matrix_path(path: str) -> MetricType:
@@ -26,7 +46,7 @@ def decode_matrix_path(path: str) -> MetricType:
 
 
 def encode_time_series_path(from_agent, to_agent: AgentID) -> str:
-    return f"{TIME_SERIES}?from={from_agent}&to={to_agent}"
+    return f"{Route.TIME_SERIES.value}?from={from_agent}&to={to_agent}"
 
 
 def decode_time_series_path(path: str) -> Tuple[AgentID, AgentID]:
