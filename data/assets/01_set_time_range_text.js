c1f71c5e506a676771543295823a39e1e850f3d2
Mateusz Midor <3demaniac@gmail.com>
2021-10-14 13:06:19 +0200

    Refactor javascript code - group in files by responsibility

---
+++ data/assets/01_set_time_range_text.js
@@ -0,0 +1,26 @@
+/* Set the time range bar text to provided timestamps, in local time */
+
+// getDataTimestamp returns test results timestamp from domDateTimeElement's title
+function getDataTimestamp(domDateTimeElement) {
+    const isoDateTimeStr =  domDateTimeElement.title;
+    if (isoDateTimeStr == "") {
+        return "<unknown>"
+    }
+    return new Date(isoDateTimeStr).toLocaleString();
+}
+
+// setTimestampsToLocalTime sets the test data timestamp low and high values
+function setTimestampsToLocalTime() {
+    var domTimestampLowElement = document.getElementById('timestamp-low');
+    var domTimestampHighElement = document.getElementById('timestamp-high');
+    if (domTimestampLowElement && domTimestampHighElement) {
+        const serverDataTimestampLow = getDataTimestamp(domTimestampLowElement);
+        domTimestampLowElement.textContent = serverDataTimestampLow
+        const serverDataTimestampHigh= getDataTimestamp(domTimestampHighElement);
+        domTimestampHighElement.textContent = serverDataTimestampHigh
+        return
+    }
+    setTimeout(setTimestampsToLocalTime, 100)
+}
+
+setTimeout(setTimestampsToLocalTime, 100)
--- data/assets/02_reload_on_navigating_back.js
+++
@@ -1,4 +0,0 @@
-// Reload the page on navigating Back - to ensure that presented data is up to date with cache
-window.onbeforeunload = function() {
-    location.reload();
-}
